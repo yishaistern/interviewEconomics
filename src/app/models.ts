8eb2b3e4cb9233286f7b44ea99d310a5bfc52788
yishaistern <yishainstern@gmail.com>
2019-12-25 00:24:48 +0200

    add init state

---
+++ src/app/models.ts
@@ -0,0 +1,18 @@
+export interface RowData {
+    Segment: string;
+    Country: string;
+    Product: string;
+    Discount_Band: string;
+    Units_Sold: string;
+    Manufacturing_Price: string;
+    Sale_Price: string;
+    Gross_Sales: string;
+    Discounts: string;
+    Sales: string;
+    COGS: string;
+    Profit: string;
+    Date: string;
+    Month_Number: number;
+    Month_Name: string;
+    Year: number;
+}
